668d29fd7359a3d5c53ed45ad9c5f320dbbb1f5a
Andrew Rowley <andrew.rowley160@outlook.com>
2026-02-16 02:25:02 -0500

    Shorten member variable names in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,79 +20,80 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
   // Robot subsystems
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final ShooterSubsystem m_robotShooter = new ShooterSubsystem();
-  private final IntakeSubsystem m_robotIntake = new IntakeSubsystem();
+  private final DriveSubsystem m_drive = new DriveSubsystem();
+  private final ShooterSubsystem m_shooter = new ShooterSubsystem();
+  private final IntakeSubsystem m_intake = new IntakeSubsystem();
 
   // Operator controllers
-  private final CommandXboxController m_pilotController = new CommandXboxController(0);
-  private final CommandXboxController m_gunnerController = new CommandXboxController(1);
+  private final CommandXboxController m_pilot = new CommandXboxController(0);
+  private final CommandXboxController m_gunner = new CommandXboxController(1);
 
   public RobotContainer() {
     configureBindings();
 
-    m_robotDrive.setDefaultCommand(getDriveCommand());
+    m_drive.setDefaultCommand(getDriveCommand());
 
   }
 
   private void configureBindings() {
     // Pilot bindings
-    m_pilotController.x().whileTrue(
-      m_robotDrive.run(m_robotDrive::setX)); // Stabalize robot
+    // Set wheels to X formation (X)
+    m_pilot.x().whileTrue(
+      m_drive.run(m_drive::setX)); 
 
     // Gunner bindings
     // *Shoot fuel (Right Trigger)
-    m_gunnerController.rightTrigger().whileTrue(
+    m_gunner.rightTrigger().whileTrue(
       shootFuel());
 
     // *Reverse shooter and intake (Left Trigger)
-    m_gunnerController.leftTrigger().whileTrue(
+    m_gunner.leftTrigger().whileTrue(
       reverseShooterAndIntake());
 
     // *Start intake routine (Right Bumper)
-    m_gunnerController.rightBumper().onTrue(
+    m_gunner.rightBumper().onTrue(
       startIntakeRoutine());
 
     // *Stop intake routine (Left Bumper)
-    m_gunnerController.leftBumper().onTrue(
+    m_gunner.leftBumper().onTrue(
       stopIntakeRoutine());
   }
 
   private Command shootFuel() {
     return new ParallelCommandGroup(
-        m_robotShooter.runLauncher(LauncherSpeed.MAX_FORWARD),
-        m_robotShooter.runFeeder(FeederSpeed.MAX_FORWARD));
+        m_shooter.runLauncher(LauncherSpeed.MAX_FORWARD),
+        m_shooter.runFeeder(FeederSpeed.MAX_FORWARD));
   }
 
   private Command reverseShooterAndIntake() {
     return new ParallelCommandGroup(
-        m_robotShooter.runLauncher(LauncherSpeed.MAX_REVERSE),
-        m_robotShooter.runFeeder(FeederSpeed.MAX_REVERSE),
-        m_robotIntake.runIntake(IntakeSpeed.MAX_REVERSE));
+        m_shooter.runLauncher(LauncherSpeed.MAX_REVERSE),
+        m_shooter.runFeeder(FeederSpeed.MAX_REVERSE),
+        m_intake.runIntake(IntakeSpeed.MAX_REVERSE));
   }
 
   private Command startIntakeRoutine() {
     return new ParallelCommandGroup(
-        m_robotShooter.runLauncher(LauncherSpeed.INTAKE_SPEED),
-        m_robotIntake.runIntake(IntakeSpeed.MAX_FORWARD));
+        m_shooter.runLauncher(LauncherSpeed.INTAKE_SPEED),
+        m_intake.runIntake(IntakeSpeed.MAX_FORWARD));
   }
 
   private Command stopIntakeRoutine() {
     return new ParallelCommandGroup(
-        m_robotShooter.runLauncher(LauncherSpeed.STOP),
-        m_robotIntake.runIntake(IntakeSpeed.STOP));
+        m_shooter.runLauncher(LauncherSpeed.STOP),
+        m_intake.runIntake(IntakeSpeed.STOP));
   }
 
   public Command getDriveCommand() {
     // The left stick controls translation of the robot.
     // Turning is controlled by the X axis of the right stick.
     return new RunCommand(
-        () -> m_robotDrive.drive(
-            -MathUtil.applyDeadband(m_pilotController.getLeftY(), OperatorConstants.kPilotControllerDeadband),
-            -MathUtil.applyDeadband(m_pilotController.getLeftX(), OperatorConstants.kPilotControllerDeadband),
-            -MathUtil.applyDeadband(m_pilotController.getRightX(), OperatorConstants.kPilotControllerDeadband),
+        () -> m_drive.drive(
+            -MathUtil.applyDeadband(m_pilot.getLeftY(), OperatorConstants.kPilotControllerDeadband),
+            -MathUtil.applyDeadband(m_pilot.getLeftX(), OperatorConstants.kPilotControllerDeadband),
+            -MathUtil.applyDeadband(m_pilot.getRightX(), OperatorConstants.kPilotControllerDeadband),
             true),
-        m_robotDrive);
+        m_drive);
   }
 
   public Command getAutonomousCommand() {
